3f052be8efe7e31d13a2a0a58661b4d895d8e3c2
danundso <123031340+danundso@users.noreply.github.com>
2023-02-01 15:29:22 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,5 @@
+function pink(){ document.body.style.background = "pink"; }
+function sky(){ document.body.style.background = "skyblue"; }
 
+<p onclick="pink()" style="padding:10px;background:pink">Pink</p>
+<p onclick="sky()" style="padding:10px;background:skyblue">Sky</p>
