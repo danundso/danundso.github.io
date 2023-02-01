9e40fb29dba396442b83542d27ecf65094c78ed1
danundso <123031340+danundso@users.noreply.github.com>
2023-02-01 16:19:48 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,9 +1,14 @@
 d = document
 
 function pink(){ 
-  title = d.getElementById("title");               
+  title = d.getElementById("title");  
+  me = d.getElementById("me");
 }
 function change(){
   title.innerHTML = "Danny Anundson";
 
+}
+
+function pic(){
+  me.innerHTML.src = "bathroom.jpg"
 }
